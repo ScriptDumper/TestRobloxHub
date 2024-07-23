134746532b82a9a762787348c01d228c90981a4a
ScriptDumper <118000363+ScriptDumper@users.noreply.github.com>
2024-07-23 14:45:19 +0900

    Delete Scripts/main.lua

--- Scripts/main.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 2248408710 then
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/ScriptDumper/TestRobloxHub/main/Scripts/Games/DS.lua'),true))()
-else
-    game.Players.LocalPlayer:Kick("Game Not Supported! | Emoji Hub")
-end
